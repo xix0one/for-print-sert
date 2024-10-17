1cd412d30cee250c9965b1e16280b6cd3b9e5a13
xix0one <petrovalex2014@ya.ru>
2024-10-17 20:15:53 +0800

    modified script, deleted jpg

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 
+names = []
 
-# open txt file
-names = open('names.txt', 'r', encoding='utf-8') 
+# open txt file and save names in array
+with open('names.txt', 'r', encoding='utf-8') as n:
+    for i, line in enumerate(n):
+        names.append(line)
 
 
 # open and read html file
@@ -14,35 +17,22 @@ with open('index.html', 'r', encoding='utf-8') as html_file:
 soup = BeautifulSoup(html_content, 'html.parser') 
 htmlNames = soup.find_all('span', class_='default')
 
-
-size = 2
 count = 0
 
-
-while (count != size):
-
-    for i in range(count):
-        for n in names:
-            for html in htmlNames:
-                html.string = n
-
-    with open(f'print-it/index_updated{count}.html', 'w', encoding='utf-8') as file:
+def add(count):
+     with open(f'print-it/index_updated{count}.html', 'w', encoding='utf-8') as file:
         file.write(str(soup))
 
-    count += 1
+for n in range(len(names)):
+    for html in htmlNames:
+            if count < len(names):
+                html.string = names[count]
+                count += 1
+            else:
+                break
+    add(count)
 
-# close file
-names.close()
+if len(names) % 2 != 0:
+    print("*Последний файл имеет лишний сертификат*")
 
-
-
-
-# end
-
-
-
-# for name in names:
-#     print(name.get_text())
-
-# for name in names:
-#     name.string = "aaa"
+print("Успешно завершено")
