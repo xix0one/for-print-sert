2f62f9f6592c244c36767efc1c29b5c93e337c9a
xix0one <petrovalex2014@ya.ru>
2024-11-02 13:03:56 +0800

    update

--- main.py
+++ main.py
@@ -73,11 +73,13 @@ def onePage():
 
         with open(f'oneImage_updated.html', 'w', encoding='utf-8') as file:
             file.write(str(soup))
-        iteration += 1    
+        iteration += 1
+        print("Лист №:", iteration, "Добавлено: ", names[n], end="")   
 
     for n in range(len(names)):
         add(n)
 
+    print()
 
 # --------------------------------------------
 
@@ -87,75 +89,68 @@ def twoPages():
     with open('twoImages.html', 'r', encoding='utf-8') as html_file: 
         html_content = html_file.read()
 
-
     # parse html file
     soup = BeautifulSoup(html_content, 'html.parser') 
-    htmlNames = soup.find_all('span', class_='default')
+    htmlDefault = soup.find_all('span', class_='default')
 
-    def add(count):
+    def add(n):
         global iteration
-        new_names = names[count:count + 2]  # Get the next two names to add in each iteration
-        if count < 2:
-            index = 0
-            for html in htmlNames:
-                html.string = new_names[index]
-                if len(names) > 1:
-                    index += 1
-        else:
-            flag = True 
 
+        if n == 0:
+            for html in htmlDefault:
+                html.string = names[n].rstrip()
+
+                iteration += 1
+                print("Лист №:", iteration, "Добавлено: ", names[n], end="")
+            if len(names) > 1:
+                br = soup.find('br')
+            
+                pName = soup.new_tag('p', attrs={'class': 'name'})  
+                defaultSpan = soup.new_tag('span', attrs={'class': 'default'}) 
+                defaultSpan.string = names[1].rstrip() 
+
+                pName.append(defaultSpan)  
+                br.insert_after(pName)
+
+                print("Лист №:", iteration, "Добавлено: ", names[1], end="")
+        else:
+            space_div = soup.new_tag('div', attrs={'class': 'space'})
             new_div = soup.new_tag('div')
             br = soup.new_tag('br')
 
-            for name in new_names:
-                
-                p1 = soup.new_tag('p', attrs={'class': 'name'})
-                span1 = soup.new_tag('span', attrs={'class': 'default'})
-                span1.string = name.rstrip()  # Add the name from the file
-                p1.append(span1)
-                
-                new_div.append(p1)
+            p1 = soup.new_tag('p', attrs={'class': 'name'})
+            spanDefault1 = soup.new_tag('span', attrs={'class': 'default'})
+            spanDefault1.string = names[n].rstrip()
+            p1.append(spanDefault1)
+            new_div.append(p1)
+            new_div.append(br)
 
-                if flag:
-                    new_div.append(br)
-                    flag = False
-                else:
-                    flag = True
-            
+            iteration += 1
+            print("Лист №:", iteration, "Добавлено: ", names[n], end="")
+
+            if n + 1 < len(names):
+                p2 = soup.new_tag('p', attrs={'class': 'name'})
+                spanDefault2 = soup.new_tag('span', attrs={'class': 'default'})
+                spanDefault2.string = names[n + 1].rstrip()
+                p2.append(spanDefault2)
+                new_div.append(p2)
+
+                print("Лист №:", iteration, "Добавлено: ", names[n+1], end="")
+        
             soup.body.append(new_div)
-
-            space_div = soup.new_tag('div', attrs={'class': 'space'})
             soup.body.append(space_div)
 
         with open(f'twoImages_updated.html', 'w', encoding='utf-8') as file:
             file.write(str(soup))
-            iteration += 1
-
-        print("Лист №:", iteration, "Добавлено: ", new_names)
 
     for n in range(0, len(names), 2):
-        global count
-        add(count)
-        if count < len(names):
-            count += 2
-        else:
-            break
+        add(n)
+        print()
 
-    if len(names) % 2 != 0:
-        print("\nВсего добавлено: ", count - 1)
-    else:
-        print("\nВсего добавлено: ", count)
-
-
-
-print("\nСколько листов на странице (1 или 2)?: ", end="")
+print("\nСколько шаблонов на странице (1 или 2)?: ", end="")
 choose = int(input())
 
-if choose == 1:
-    onePage()
-elif choose == 2:
-    twoPages()
+onePage() if choose == 1 else twoPages()
 
-
-print("Всего страниц:", iteration)
+print("\nВсего страниц:", iteration)
 print("Успешно завершено")
